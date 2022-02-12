496568eea2ab5a383e9e7d2e74d434c9418c1edc
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-02-12 09:15:38 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -50,7 +50,7 @@ void quicksort(int a, int b, int* x) {
         return;
     }
     int m, k, l, r;
-    m = rand() % (b - a + 1) + a;
+    m = (rand()*rand()) % (b - a + 1) + a;
     k = x[m];
     l = a - 1;
     r = b + 1;
